f7f9dad43607d8f53ebbec2c6b07adcfb8403c06
NenadJovicic <n.jovicic357@gmail.com>
2024-12-15 12:54:35 +0100

    find spot by office id

--- src/charging-spot/charging-spot.controller.ts
+++ src/charging-spot/charging-spot.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ChargingSpot } from '../entities/charging-spot.entity';
 import { ChargingSpotValidator } from '../validators/charging-spot.validator';
 
@@ -7,5 +7,10 @@ export class ChargingSpotController {
   @Post()
   public async addChargingSpot(@Body() chargingSpot: ChargingSpotValidator): Promise<ChargingSpot> {
     return ChargingSpot.create({ ...chargingSpot });
+  }
+
+  @Get('/office/:officeId')
+  public async getChargingSpotsForOffice(@Param('officeId') officeId: string) {
+    return ChargingSpot.findAll({ where: { officeId } });
   }
 }
